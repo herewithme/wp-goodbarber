eb61c19d7a0adc76b0ff06ba143c023e4588509d
Amaury Balmer <amaury@beapi.fr>
2020-04-30 20:04:32 +0200

    code restyling

--- models/category.php
+++ models/category.php
@@ -1,27 +1,27 @@
 <?php
 
 class GB_JSON_API_Category {
-  
-  var $id;          // Integer
-  var $slug;        // String
-  var $title;       // String
-  var $description; // String
-  var $parent;      // Integer
-  var $post_count;  // Integer
-  
-  function __construct($wp_category = null) {
-    if ($wp_category) {
-      $this->import_wp_object($wp_category);
-    }
-  }
-  
-  function import_wp_object($wp_category) {
-    $this->id = (int) $wp_category->term_id;
-    $this->slug = $wp_category->slug;
-    $this->title = $wp_category->name;
-    $this->description = $wp_category->description;
-    $this->parent = (int) $wp_category->parent;
-    $this->post_count = (int) $wp_category->count;
-  }
-  
+
+	public $id;          // Integer
+	public $slug;        // String
+	public $title;       // String
+	public $description; // String
+	public $parent;      // Integer
+	public $post_count;  // Integer
+
+	public function __construct( $wp_category = null ) {
+		if ( $wp_category ) {
+			$this->import_wp_object( $wp_category );
+		}
+	}
+
+	public function import_wp_object( $wp_category ) {
+		$this->id          = (int) $wp_category->term_id;
+		$this->slug        = $wp_category->slug;
+		$this->title       = $wp_category->name;
+		$this->description = $wp_category->description;
+		$this->parent      = (int) $wp_category->parent;
+		$this->post_count  = (int) $wp_category->count;
+	}
+
 }
